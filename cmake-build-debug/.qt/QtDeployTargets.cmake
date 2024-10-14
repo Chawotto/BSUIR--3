1d7681dbab5622cf5d8e43ce30e9f49d3b7e7ea2
Esepenok Alexandr <145503317+Chawotto@users.noreply.github.com>
2024-10-14 13:16:39 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MorrsQT_FILE C:/Users/alexe/Desktop/MorrsQT/cmake-build-debug/MorrsQT.exe)
-set(__QT_DEPLOY_TARGET_MorrsQT_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MorrsQT_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
